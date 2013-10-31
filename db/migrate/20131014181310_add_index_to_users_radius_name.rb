84637601ba1cdadcad487f2105bd9fb1e881581c
alohara <alohara@gmail.com>
2013-10-30 22:13:15 -0700

    Test migration change

--- db/migrate/20131014181310_add_index_to_users_radius_name.rb
+++ db/migrate/20131014181310_add_index_to_users_radius_name.rb
@@ -1,6 +1,7 @@
 class AddIndexToUsersRadiusName < ActiveRecord::Migration
   def change
     add_index :users, :radius_name, unique: true
+  	add_index :users, :remember_token
 	add_index :user_profiles, :email, unique: true
   end
 end
